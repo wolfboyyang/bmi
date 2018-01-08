c226a018d52f2b751d25bc510eb19add23909627
Wei Yang <youi1987@gmail.com>
2018-01-08 14:31:44 +0800

    modify Dockerfile for huaweicloud.

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CMDVAR="-Djava.security.egd=file:/dev/./urandom","java -agentlib:jdwp=transport=dt_socket,address=0:8000,server=y,suspend=n -jar"
-java $CMDVAR -jar ./${project.parent.artifactId}/$ARTIFACT_ID-${project.version}-exec.jar
+java $CMDVAR -jar ./webapp-1.0-SNAPSHOT-exec.jar
